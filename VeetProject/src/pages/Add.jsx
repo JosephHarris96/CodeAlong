5c278e6df4ce8717b5e80aac7c04d076445b56d9
Joseph Harris <josephellisharris@gmail.com>
2023-10-10 16:37:16 +0100

    Saving Files

--- VeetProject/src/pages/Add.jsx
+++ VeetProject/src/pages/Add.jsx
@@ -1,16 +1,21 @@
-import React from "react"
+import React from "react";
 import Typography from '@mui/material/Typography';
-
+import CarForms from "../forms/CarForms";
 
 function Add() {
+    const handleCarFormSubmit = (data) => {
+        // Handle the form submission logic here
+        console.log("Form submitted with data:", data);
+    }
+
     return (
         <>
-        <Typography variant="h2" component="h1">
-            Add Car
-        </Typography>
-        
+            <Typography variant="h2" component="h1">
+                Add Car
+            </Typography>
+            <CarForm submitHandler={handleCarFormSubmit} />
         </>
     )
 }
 
-export default Add
+export default Add;
